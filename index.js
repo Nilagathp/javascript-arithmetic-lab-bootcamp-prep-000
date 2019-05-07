108dcbbbbc49bac612bba958c36c2cdc0c6b33fa
empathetic-parameter-3158 <nilagathp@gmail.com>
2019-05-07 06:41:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-1 + 80
-60 - 40
-2 * 3.4
-5.0 / 2.5
+1 + 80;
+60 - 40;
+2 * 3.4;
+5.0 / 2.5;
+
+var number = 10;
+
+function add5(){
+  number += 5;
+}
+
+function divideBy3 () {
+  number /= 3;
+}
